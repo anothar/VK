b0a7f9c21151ea2d3137dcc339a65d478116877e
Alexey <alexey23081985@gmail.com>
2016-04-20 19:26:41 +0500

    video

--- vkProject/vkProject/Controls/ctrVideo.xaml.cs
+++ vkProject/vkProject/Controls/ctrVideo.xaml.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Media.Animation;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -23,7 +24,80 @@ namespace VkAPI.Controls
 		public ctrVideo()
 		{
 			InitializeComponent();
+			description.Opacity = 0.0;
+			panel.Opacity = 0.0;
 		}
-		
+		public string Title
+		{
+			get { return title.Content.ToString(); }
+			set { title.Content = value; }
+		}
+		public uint Views
+		{
+			get { return Convert.ToUInt32(views.Content.ToString()); }
+			set { views.Content = Convert.ToString(value); }
+		}
+		public string Description
+		{
+			get { return description.Text; }
+			set { description.Text = value; }
+		}
+		public string Url
+		{
+			get { return Url; }
+			set { Url = value; }
+		}
+		public string Photo
+		{
+			get { return photo.Source.ToString(); }
+			set { photo.Source = new BitmapImage(new Uri(value)); }
+		}
+
+		private void StackPanel_MouseEnter(object sender, MouseEventArgs e)
+		{
+			DoubleAnimation da = new DoubleAnimation();
+			da.From = 0.0;
+			da.To = 1.0;
+			da.Duration = new Duration(TimeSpan.FromMilliseconds(200));
+			panel.BeginAnimation(OpacityProperty, da);
+			description.BeginAnimation(OpacityProperty, da);
+		}
+		private void panel_MouseLeave(object sender, MouseEventArgs e)
+		{
+			DoubleAnimation da = new DoubleAnimation();
+			da.From = 1.0;
+			da.To = 0.0;
+			da.BeginTime = TimeSpan.FromMilliseconds(1000);
+			da.Duration = new Duration(TimeSpan.FromMilliseconds(200));
+			panel.BeginAnimation(OpacityProperty, da);
+			description.BeginAnimation(OpacityProperty, da);
+		}
+		private void play_MouseEnter(object sender, MouseEventArgs e)
+		{
+			DoubleAnimation da = new DoubleAnimation();
+			da.From = 0.65;
+			da.To = 1.0;
+			da.Duration = new Duration(TimeSpan.FromMilliseconds(200));
+			play.BeginAnimation(OpacityProperty, da);
+		}
+		private void play_MouseLeave(object sender, MouseEventArgs e)
+		{
+			DoubleAnimation da = new DoubleAnimation();
+			da.From = 1.0;
+			da.To = 0.65;
+			da.Duration = new Duration(TimeSpan.FromMilliseconds(200));
+			play.BeginAnimation(OpacityProperty, da);
+		}
+
+		private void play_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+		{
+			DoubleAnimation da = new DoubleAnimation();
+			da.Duration = new Duration(TimeSpan.FromMilliseconds(200));
+			da.From = 1.0;
+			da.To = 0.0;
+			play.BeginAnimation(OpacityProperty, da);
+			player.Visibility = Visibility.Visible;
+			play.Visibility = Visibility.Hidden;
+		}
 	}
 }
